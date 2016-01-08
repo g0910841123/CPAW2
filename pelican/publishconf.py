e56744b1711fcd46ec718f2ff3a87f6932f4355a
g0910841123 <g0921887186@yahoo.com.tw>
2016-01-08 12:10:31 +0800

    40423114

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = 'KMOL'
-SITENAME = '2015FALL KMOL 課程'
+AUTHOR = '40423114'
+SITENAME = '2015FALL 40423114 CPA 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,13 +26,12 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423114作業主頁', 'https://github.com/40423114'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('github', 'http://github.com/ametaireau'),)
+          
 DEFAULT_PAGINATION = 10
 
 SITEURL = 'http://coursemdetw.github.io/reveal'
@@ -50,8 +49,9 @@ DISQUS_SITENAME = "2015fall"
 #GOOGLE_ANALYTICS = ""
 
 # 必須絕對目錄或相對於設定檔案所在目錄
-PLUGIN_PATHS = ['./../plugin']
+PLUGIN_PATHS = ['./../../../plugin']
 PLUGINS = ['liquid_tags.notebook']
 # 目錄設定相對於 reveal 下的 content 目錄
 NOTEBOOK_DIR = 'notebook'
+
 
